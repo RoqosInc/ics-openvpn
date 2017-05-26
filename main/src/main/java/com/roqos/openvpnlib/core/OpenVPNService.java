0a344d71bc50597bf82ed320e67d15d58d16e7dc
Bradley168 <bradley.heng@pathmazing.com>
2017-05-26 09:05:54 +0700

    Change status icon

--- main/src/main/java/com/roqos/openvpnlib/core/OpenVPNService.java
+++ main/src/main/java/com/roqos/openvpnlib/core/OpenVPNService.java
@@ -217,21 +217,21 @@ public class OpenVPNService extends VpnService implements StateListener, Callbac
     private int getIconByConnectionStatus(ConnectionStatus level) {
         switch (level) {
             case LEVEL_CONNECTED:
-                return R.drawable.ic_stat_vpn;
+                return R.drawable.roqos_vpn_enabled;
             case LEVEL_AUTH_FAILED:
             case LEVEL_NONETWORK:
             case LEVEL_NOTCONNECTED:
-                return R.drawable.ic_stat_vpn_offline;
+                return R.drawable.roqos_vpn_disabled;
             case LEVEL_CONNECTING_NO_SERVER_REPLY_YET:
             case LEVEL_WAITING_FOR_USER_INPUT:
-                return R.drawable.ic_stat_vpn_outline;
+                return R.drawable.roqos_vpn;
             case LEVEL_CONNECTING_SERVER_REPLIED:
-                return R.drawable.ic_stat_vpn_empty_halo;
+                return R.drawable.roqos_vpn_disabled;
             case LEVEL_VPNPAUSED:
                 return android.R.drawable.ic_media_pause;
             case UNKNOWN_LEVEL:
             default:
-                return R.drawable.ic_stat_vpn;
+                return R.drawable.roqos_vpn_enabled;
 
         }
     }
