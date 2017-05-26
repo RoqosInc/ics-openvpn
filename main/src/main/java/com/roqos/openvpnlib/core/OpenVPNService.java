43a502d5233f6b15e92763eeb42b149df3bc4b45
Bradley168 <bradley.heng@pathmazing.com>
2017-05-26 09:10:34 +0700

    Remove Disconnect, Pause, and Resume buttons

--- main/src/main/java/com/roqos/openvpnlib/core/OpenVPNService.java
+++ main/src/main/java/com/roqos/openvpnlib/core/OpenVPNService.java
@@ -250,26 +250,26 @@ public class OpenVPNService extends VpnService implements StateListener, Callbac
 
             }
 
-            Intent disconnectVPN = new Intent(this, DisconnectVPN.class);
-            disconnectVPN.setAction(DISCONNECT_VPN);
-            PendingIntent disconnectPendingIntent = PendingIntent.getActivity(this, 0, disconnectVPN, 0);
-
-            nbuilder.addAction(R.drawable.ic_menu_close_clear_cancel,
-                    getString(R.string.cancel_connection), disconnectPendingIntent);
-
-            Intent pauseVPN = new Intent(this, OpenVPNService.class);
-            if (mDeviceStateReceiver == null || !mDeviceStateReceiver.isUserPaused()) {
-                pauseVPN.setAction(PAUSE_VPN);
-                PendingIntent pauseVPNPending = PendingIntent.getService(this, 0, pauseVPN, 0);
-                nbuilder.addAction(R.drawable.ic_menu_pause,
-                        getString(R.string.pauseVPN), pauseVPNPending);
-
-            } else {
-                pauseVPN.setAction(RESUME_VPN);
-                PendingIntent resumeVPNPending = PendingIntent.getService(this, 0, pauseVPN, 0);
-                nbuilder.addAction(R.drawable.ic_menu_play,
-                        getString(R.string.resumevpn), resumeVPNPending);
-            }
+//            Intent disconnectVPN = new Intent(this, DisconnectVPN.class);
+//            disconnectVPN.setAction(DISCONNECT_VPN);
+//            PendingIntent disconnectPendingIntent = PendingIntent.getActivity(this, 0, disconnectVPN, 0);
+//
+//            nbuilder.addAction(R.drawable.ic_menu_close_clear_cancel,
+//                    getString(R.string.cancel_connection), disconnectPendingIntent);
+//
+//            Intent pauseVPN = new Intent(this, OpenVPNService.class);
+//            if (mDeviceStateReceiver == null || !mDeviceStateReceiver.isUserPaused()) {
+//                pauseVPN.setAction(PAUSE_VPN);
+//                PendingIntent pauseVPNPending = PendingIntent.getService(this, 0, pauseVPN, 0);
+//                nbuilder.addAction(R.drawable.ic_menu_pause,
+//                        getString(R.string.pauseVPN), pauseVPNPending);
+//
+//            } else {
+//                pauseVPN.setAction(RESUME_VPN);
+//                PendingIntent resumeVPNPending = PendingIntent.getService(this, 0, pauseVPN, 0);
+//                nbuilder.addAction(R.drawable.ic_menu_play,
+//                        getString(R.string.resumevpn), resumeVPNPending);
+//            }
 
 
             //ignore exception
